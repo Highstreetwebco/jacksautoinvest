ea69ed714e3268dff95fe67d7d84342b9253b4cf
jtroth1997 <troth.jack@yahoo.co.uk>
2026-07-28 16:43:19 +0100

    Connect public Supabase project configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.JAI_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://wupecgimrmgmdzqvrkdr.supabase.co",
+  supabaseAnonKey: "sb_publishable_kwfTugxOdeW7BMuZWZ_lFg_7yR6k1zT"
 };
